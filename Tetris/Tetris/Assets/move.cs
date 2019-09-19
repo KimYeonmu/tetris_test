1bf8568069243c62483971f63621c42dec51b251
Unknown <rladusan135@naver.com>
2019-09-19 20:48:50 +0900

    Add Move

---
+++ Tetris/Tetris/Assets/move.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	void Start ()
+    {
+		transform.position = new Vector3(1,1);
+    }
+	
+	void Update ()
+	{
+	    Vector3 pos = transform.position;
+		transform.position = new Vector3(pos.x,pos.y-0.1f);
+	}
+}
