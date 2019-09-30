348fba6ecf79332144ca8bbdaa3876cdff31bb60
Unknown <rladusan135@naver.com>
2019-09-30 20:37:11 +0900

    Add Collider

---
+++ Tetris/Tetris/Assets/collide.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collide : MonoBehaviour
+{
+    public BoxCollider2D Collider;
+
+	void Start ()
+	{
+	    Collider = GetComponent<BoxCollider2D>();
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+    public void OnCollisionEnter2D(Collision2D collider)
+    {
+        Debug.Log("enter");
+    }
+
+    public void OnCollisionStay2D(Collision2D collider)
+    {
+        Debug.Log("stay");
+    }
+
+    public void OnCollisionExit2D(Collision2D collider)
+    {
+        Debug.Log("exit");
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag != "team")
+            Destroy(col.gameObject);
+    }
+
+    public void OnTriggerStay2D(Collider2D collider)
+    { 
+        Debug.Log("trigger stay");
+    }
+
+    public void OnTriggerExit2D(Collider2D collider)
+    {
+        Debug.Log("trigger exit");
+    }
+}
--- Tetris/Tetris/Assets/move.cs
+++ Tetris/Tetris/Assets/move.cs
@@ -6,12 +6,52 @@ public class move : MonoBehaviour {
 
 	void Start ()
     {
-		transform.position = new Vector3(1,1);
     }
 	
 	void Update ()
 	{
-	    Vector3 pos = transform.position;
-		transform.position = new Vector3(pos.x,pos.y-0.1f);
+	    //Vector3 pos = transform.position;
+		// transform.position = new Vector3(pos.x,pos.y-0.1f);
+
+        //Debug.Log(transform.position);
+
+        //var pos = transform.position;
+
+        //transform.position = new Vector3(pos.x+0.1f,0,0);
+
+        //transform.Translate(new Vector3(0.1f,0,0));
+
+        //transform.
+
+	    //var rotate = transform.localEulerAngles;
+        //transform.localEulerAngles = new Vector3(0,0,rotate.z+3);
+
+	    var pos = transform.position;
+
+	    if (Input.GetKey(KeyCode.LeftArrow))
+	        pos.x -= 0.1f;
+
+	    if (Input.GetKey(KeyCode.UpArrow))
+	        pos.y += 0.1f;
+
+	    if (Input.GetKey(KeyCode.RightArrow))
+	        pos.x += 0.1f;
+
+	    if (Input.GetKey(KeyCode.DownArrow))
+	        pos.y -= 0.1f;
+
+	    transform.position = pos;
+	    //transorm.Rotate(new Vector3(0,0,3));
+	    /*
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.position 
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        */
+
+
 	}
 }
