e778243721e0b70011dcd9759d4c58adc8c1f49e
Unknown <rladusan135@naver.com>
2019-10-07 21:08:15 +0900

    Add Bullet

---
+++ Tetris/Tetris/Assets/bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float Speed = 5;
+
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Vector3 pos = transform.position;
+	    pos.x += Speed * Time.deltaTime;
+	    transform.position = pos;
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("enemy"))
+        {
+            Destroy(collider.gameObject);
+        }
+    }
+}
--- Tetris/Tetris/Assets/collide.cs
+++ Tetris/Tetris/Assets/collide.cs
@@ -16,21 +16,7 @@ public class collide : MonoBehaviour
 		
 	}
 
-    public void OnCollisionEnter2D(Collision2D collider)
-    {
-        Debug.Log("enter");
-    }
-
-    public void OnCollisionStay2D(Collision2D collider)
-    {
-        Debug.Log("stay");
-    }
-
-    public void OnCollisionExit2D(Collision2D collider)
-    {
-        Debug.Log("exit");
-    }
-
+    
     public void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag != "team")
